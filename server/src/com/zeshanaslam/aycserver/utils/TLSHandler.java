6a1ba599fd5f5ac60e3e97b20e2fb4731e8179d3
Zeshan Aslam <zeshan12aslam@gmail.com>
2015-12-27 14:47:25 -0500

    Base android files and more
    Android:
    - Base android files
    - Created login layout
    
    Server:
    - Using X509TrustManager now

--- server/src/com/zeshanaslam/aycserver/utils/TLSHandler.java
+++ server/src/com/zeshanaslam/aycserver/utils/TLSHandler.java
@@ -7,11 +7,14 @@ import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509TrustManager;
 
 import com.sun.net.httpserver.HttpsConfigurator;
 import com.zeshanaslam.aycserver.Main;
@@ -31,7 +34,14 @@ public class TLSHandler {
 		keyManager.init(keyStore, ctPassword);
 
 		SSLContext sslContext = SSLContext.getInstance("TLS");
-		sslContext.init(keyManager.getKeyManagers(), null, null);
+		sslContext.init(keyManager.getKeyManagers(), new X509TrustManager[]{new X509TrustManager(){ 
+			public void checkClientTrusted(X509Certificate[] chain, 
+					String authType) throws CertificateException {} 
+			public void checkServerTrusted(X509Certificate[] chain, 
+					String authType) throws CertificateException {} 
+			public X509Certificate[] getAcceptedIssuers() { 
+				return new X509Certificate[0]; 
+			}}}, new SecureRandom());
 
 		return new HttpsConfigurator(sslContext);
 	}
